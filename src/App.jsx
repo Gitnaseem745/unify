a8690f20eb183c2b7d958e9dde6ac0bcb7105912
Naseem Ansari <mrnaseem745@gmail.com>
2024-10-08 00:41:28 +0530

    #unify project started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+   <>
+   <div className="flex items-center h-screen justify-center text-7xl leading-tight">
+   Define <span className="text-white px-2 font-bold">Right Width</span> and <span className="text-white px-2 font-bold">Height</span> for App's All Pages
+   </div>
+   </>
+  )
+}
+
+export default App
